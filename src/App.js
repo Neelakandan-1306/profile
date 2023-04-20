fff452d53e0a7be66e8dec5e9885592c9140bb77
Neelakandan-1306 <neelakandan309@gmail.com>
2023-04-20 14:30:05 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './component/About';
+import Contact from './component/Contact';
+import ScrollToTop from './component/Scroll';
+import Social from './component/Social';
+import Welcome from './component/Welcome';
+import Header from './component/header';
+import Skills from './component/skills';
+import OuterCircle from './component/skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Welcome/>
+      <Skills/>
+      <About />
+      <Contact/>
+      <Social/>
+      <ScrollToTop/>
     </div>
   );
 }
